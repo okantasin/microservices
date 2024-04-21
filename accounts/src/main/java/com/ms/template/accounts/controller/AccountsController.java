5aa767bde6b6be32c01407f507a681d4e1283935
okantasin <okan.tasin@hotmail.com>
2024-04-21 13:44:11 -0700

    1
    add accounts

---
+++ accounts/src/main/java/com/ms/template/accounts/controller/AccountsController.java
@@ -0,0 +1,15 @@
+package com.ms.template.accounts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class AccountsController {
+
+    @GetMapping("sayHello")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
+
